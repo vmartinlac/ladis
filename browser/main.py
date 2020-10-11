2257ac58260fe62605d1d957d618e009b318fe59
Victor Martin Lac <victormartinlac@gmail.com>
2020-10-11 12:02:28 +0200

    WIP

--- browser/main.py
+++ browser/main.py
@@ -1,13 +1,16 @@
 
 import wsgiref.simple_server
 import pyramid.config
+import views
 
 if __name__ == '__main__':
     cfg = pyramid.config.Configurator()
     cfg.add_route('index', '/')
     cfg.add_route('match', '/match/{match}')
+    cfg.add_route('static', '/static')
     cfg.include('pyramid_jinja2')
-    cfg.scan('views')
+    cfg.add_static_view(name='static', path=views.static_directory)
+    cfg.scan(views)
     app = cfg.make_wsgi_app()
     server = wsgiref.simple_server.make_server('0.0.0.0', 8080, app)
     server.serve_forever()
--- browser/views.py
+++ browser/views.py
@@ -1,16 +1,25 @@
 
+import os
 import bson
 import pymongo
 import pyramid.view
 
 client = pymongo.MongoClient("mongodb://192.168.1.2")
-database = client['ladis']
+database = client.ladis
+collection = database.matches
+
+static_directory = os.path.abspath('static')
+overwrite = False
+match_limit = None
 
 @pyramid.view.view_config(route_name='index', renderer='index.jinja2')
-def v_index(request):
+def view_index(request):
 
-    matches = database['matches'].find({}, {'_id':1, 'result':1, 'agent':1, 'start_timestamp':1})
-    matches = list(matches)
+    pipeline = list()
+    pipeline.append({'$sort':{'start_timestamp':pymongo.DESCENDING}})
+
+    with collection.aggregate(pipeline) as cursor:
+        matches = list(cursor)
 
     ret = dict()
     ret['matches'] = matches
@@ -18,7 +27,7 @@ def v_index(request):
     return ret
 
 @pyramid.view.view_config(route_name='match', renderer='match.jinja2')
-def v_match(request):
+def view_match(request):
     match = database['matches'].find_one({'_id':bson.ObjectId(request.matchdict['match'])})
     return {}
 
